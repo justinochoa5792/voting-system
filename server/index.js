4f6ba7fcc58d902d70e0f7e142f6d8f175c0935e
Justin Ochoa <just.och92@gmail.com>
2023-01-16 14:00:34 -0500

    completed front end

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const dataFile = path.join(__dirname, "data.json");
 
 app.use(express.urlencoded({ extended: true }));
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  next();
+});
+
 app.get("/poll", async (req, res) => {
   let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
   const totalVotes = Object.values(data).reduce((total, n) => (total += n), 0);
